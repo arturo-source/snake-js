66b8bbc6e6b3d73ec7f0842ee18b1af4adc7b957
arturo-source <arturojuan9898@gmail.com>
2022-11-02 21:25:26 +0100

    AI knows distances and what is up,down,left,right

--- ai.js
+++ ai.js
@@ -5,39 +5,50 @@ const nnIsTrained = () => nn.outputLayer !== -1;
 
 function DefaultTrain() {
     const TOTAL_CELLS = GAME_HEIGHT / SQUARE_SIZE * GAME_WIDTH / SQUARE_SIZE;
-    const emptyState = Array(2).fill(0);
+    const emptyState = {
+        diffX: 0,
+        diffY: 0,
+        up: 0,
+        down: 0,
+        left: 0,
+        right: 0,
+    };
 
     let steps = [
-        { input: emptyState.slice(), output: { up: 1, down: 0, left: 0, right: 0 } },
-        { input: emptyState.slice(), output: { up: 0, down: 1, left: 0, right: 0 } },
-        { input: emptyState.slice(), output: { up: 0, down: 0, left: 1, right: 0 } },
-        { input: emptyState.slice(), output: { up: 0, down: 0, left: 0, right: 1 } }
+        { input: { ...emptyState }, output: { up: 1, down: 0, left: 0, right: 0 } },
+        { input: { ...emptyState }, output: { up: 0, down: 1, left: 0, right: 0 } },
+        { input: { ...emptyState }, output: { up: 0, down: 0, left: 1, right: 0 } },
+        { input: { ...emptyState }, output: { up: 0, down: 0, left: 0, right: 1 } }
     ];
 
     // add snake in center and food in top 
-    steps[0].input[0] = 0;
-    steps[0].input[1] = -SQUARE_SIZE;
+    steps[0].input.diffX = 0;
+    steps[0].input.diffY = -SQUARE_SIZE / GAME_HEIGHT;
 
     // add snake in center and food in bottom
-    steps[1].input[0] = 0;
-    steps[1].input[1] = SQUARE_SIZE;
+    steps[1].input.diffX = 0;
+    steps[1].input.diffY = SQUARE_SIZE / GAME_HEIGHT;
 
     // add snake in center and food in left
-    steps[2].input[0] = -SQUARE_SIZE;
-    steps[2].input[1] = 0;
+    steps[2].input.diffX = -SQUARE_SIZE / GAME_WIDTH;
+    steps[2].input.diffY = 0;
 
     // add snake in center and food in right
-    steps[3].input[0] = SQUARE_SIZE;
-    steps[3].input[1] = 0;
+    steps[3].input.diffX = SQUARE_SIZE / GAME_WIDTH;
+    steps[3].input.diffY = 0;
 
     return steps;
 }
 
 function CurrentGameState() {
-    const diffX = food.x - snake.body[0].x;
-    const diffY = food.y - snake.body[0].y;
-
-    return [diffX, diffY];
+    return {
+        diffX: (food.x - snake.body[0].x) / GAME_WIDTH,
+        diffY: (food.y - snake.body[0].y) / GAME_HEIGHT,
+        up: Number(snake.body.some(part => part.x === snake.body[0].x && part.y === snake.body[0].y - SQUARE_SIZE)),
+        down: Number(snake.body.some(part => part.x === snake.body[0].x && part.y === snake.body[0].y + SQUARE_SIZE)),
+        left: Number(snake.body.some(part => part.x === snake.body[0].x - SQUARE_SIZE && part.y === snake.body[0].y)),
+        right: Number(snake.body.some(part => part.x === snake.body[0].x + SQUARE_SIZE && part.y === snake.body[0].y)),
+    };
 }
 
 function CurrentTrainState() {
@@ -52,14 +63,8 @@ function CurrentTrainState() {
 
 
 snake.Train = (steps) => {
-    // Normalize the data
-    steps = steps.map(step => {
-        step.input = step.input.map(cell => cell / GAME_WIDTH);
-        return step;
-    });
-
     nn.train(steps, {
-        iterations: 100000,
+        // iterations: 100000,
         // errorThresh: 0.0001,
         log: err => console.log(err),
     });
@@ -69,10 +74,7 @@ snake.Run = () => {
     const getKeyFromBiggest = (obj) =>
         Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b);
 
-    let currentState = CurrentGameState();
-    // Normalize the data
-    currentState = currentState.map(cell => cell / GAME_WIDTH);
-
+    const currentState = CurrentGameState();
     const result = nn.run(currentState);
     console.log(result);
 
